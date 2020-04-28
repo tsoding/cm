e4922f52cc74a320660dfca7afc6cf3bb5524b80
rexim <reximkut@gmail.com>
2020-04-29 05:53:56 +0700

    Introduce Box<dyn Error> thingie

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use libc::*;
 use std::ffi::CString;
 use std::process::Command;
 use std::fs::File;
+use std::error::Error;
 
 const REGULAR_PAIR: i16 = 1;
 const CURSOR_PAIR: i16 = 2;
@@ -20,22 +21,22 @@ fn render_list(lines: &Vec<String>, cursor: usize) {
     }
 }
 
-fn main() -> Result<(), String> {
+fn main() -> Result<(), Box<dyn Error>> {
     // TODO(#2): regexp is not customizable
-    let re = Regex::new(r"^(.*?):(\d+):").map_err(|e| e.to_string())?;
+    let re = Regex::new(r"^(.*?):(\d+):")?;
 
     let mut lines: Vec<String> = Vec::new();
     let mut cursor: usize = 0;
     let mut line: String = String::new();
-    while stdin().read_line(&mut line).map_err(|e| e.to_string())? > 0 {
+    while stdin().read_line(&mut line)? > 0 {
         lines.push(line.clone());
         line.clear();
     }
 
     // NOTE: stolen from https://stackoverflow.com/a/44884859
     // TODO(#3): the terminal redirection is too hacky
-    let tty_path = CString::new("/dev/tty").map_err(|e| e.to_string())?;
-    let fopen_mode = CString::new("r+").map_err(|e| e.to_string())?;
+    let tty_path = CString::new("/dev/tty")?;
+    let fopen_mode = CString::new("r+")?;
     let file = unsafe { fopen(tty_path.as_ptr(), fopen_mode.as_ptr()) };
     let screen = newterm(None, file, file);
     set_term(screen);
@@ -57,11 +58,11 @@ fn main() -> Result<(), String> {
                 for cap in re.captures_iter(lines[cursor].as_str()) {
                     // TODO(#6): the program does not go back after exiting vim
                     Command::new("vim")
-                        .stdin(File::open("/dev/tty").map_err(|e| e.to_string())?)
+                        .stdin(File::open("/dev/tty")?)
                         .arg(format!("+{}", &cap[2]))
                         .arg(&cap[1])
-                        .spawn().map_err(|e| e.to_string())?
-                        .wait_with_output().map_err(|e| e.to_string())? ;
+                        .spawn()?
+                        .wait_with_output()? ;
                 }
             }
             113 => quit = true,
