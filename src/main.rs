f33d0c2c263f7526fef6ca626cb9d892074d71e5
rexim <reximkut@gmail.com>
2020-05-05 23:44:16 +0700

    (#7) Limit the amount of rendered lines to the size of the terminal

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,13 @@ const REGULAR_PAIR: i16 = 1;
 const CURSOR_PAIR: i16 = 2;
 
 fn render_list(lines: &[String], cursor: usize) {
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+
+    getmaxyx(stdscr(), &mut y, &mut x);
+
     // TODO(#1): captured regexp groups are not highlighted
-    for (i, line) in lines.iter().enumerate() {
+    for (i, line) in lines.iter().enumerate().take_while(|(i, _)| *i < y as usize) {
         mv(i as i32, 0);
         let pair = if i == cursor {
             CURSOR_PAIR
