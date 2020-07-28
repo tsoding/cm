0b52ddf7187078bf47afe6907315b4c85cfbe694
rexim <reximkut@gmail.com>
2020-07-29 05:51:35 +0700

    Extract EditField actions

--- src/ui.rs
+++ src/ui.rs
@@ -209,23 +209,46 @@ impl EditField {
         mv(y as i32, (x + self.cursor_x % w) as i32);
     }
 
+    pub fn insert_char(&mut self, ch: char) {
+        self.buffer.insert(self.cursor_x, ch);
+        self.cursor_x += 1;
+    }
+
+    pub fn left(&mut self) {
+        if self.cursor_x > 0 {
+            self.cursor_x -= 1
+        }
+    }
+
+    pub fn right(&mut self) {
+        if self.cursor_x < self.buffer.len() {
+            self.cursor_x += 1;
+        }
+    }
+
+    pub fn backspace(&mut self) {
+        if self.cursor_x > 0 {
+            self.cursor_x -= 1;
+            self.buffer.remove(self.cursor_x);
+        }
+    }
+
     pub fn handle_key(&mut self, key_stroke: KeyStroke) {
         if 32 <= key_stroke.key && key_stroke.key <= 126 {
-            self.buffer
-                .insert(self.cursor_x, key_stroke.key as u8 as char);
-            self.cursor_x += 1;
+            self.insert_char(key_stroke.key as u8 as char);
         }
 
         match key_stroke {
-            KeyStroke { key: KEY_RIGHT, .. } if self.cursor_x < self.buffer.len() => {
-                self.cursor_x += 1
+            KeyStroke { key: KEY_RIGHT, .. } => {
+                self.right();
             }
-            KeyStroke { key: KEY_LEFT, .. } if self.cursor_x > 0 => self.cursor_x -= 1,
+            KeyStroke { key: KEY_LEFT, .. } =>  {
+                self.left();
+            },
             KeyStroke {
                 key: KEY_BACKSPACE, ..
-            } if self.cursor_x > 0 => {
-                self.cursor_x -= 1;
-                self.buffer.remove(self.cursor_x);
+            } => {
+                self.backspace();
             }
             _ => {}
         }
