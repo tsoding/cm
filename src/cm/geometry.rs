4d2bad71c328ad7e4626995d46b38c0b30f6ee9a
rexim <reximkut@gmail.com>
2020-07-29 07:23:39 +0700

    Move rectangle splitting logic to geometry module

--- src/cm/geometry.rs
+++ src/cm/geometry.rs
@@ -6,6 +6,46 @@ pub struct Rect {
     pub h: usize,
 }
 
+impl Rect {
+    pub fn horizontal_split(self, denominator: usize) -> (Rect, Rect) {
+        assert!(denominator > 0);
+        let h = self.h / denominator * (denominator - 1);
+        (
+            Rect {
+                x: self.x,
+                y: self.y,
+                w: self.w,
+                h,
+            },
+            Rect {
+                x: self.x,
+                y: self.y + h,
+                w: self.w,
+                h: self.h - h,
+            },
+        )
+    }
+
+    pub fn vertical_split(self, denominator: usize) -> (Rect, Rect) {
+        assert!(denominator > 0);
+        let w = self.w / denominator * (denominator - 1);
+        (
+            Rect {
+                x: self.x,
+                y: self.y,
+                w,
+                h: self.h,
+            },
+            Rect {
+                x: self.x + w,
+                y: self.y,
+                w: self.w - w,
+                h: self.h,
+            },
+        )
+    }
+}
+
 #[derive(Clone, Copy)]
 pub struct Row {
     pub x: usize,
