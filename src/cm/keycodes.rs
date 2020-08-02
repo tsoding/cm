4c4f0902c806e7ca71b68265e114aee959ac410f
rexim <reximkut@gmail.com>
2020-08-02 15:45:34 +0700

    (#12) Remove redundant keycodes

--- src/cm/keycodes.rs
+++ src/cm/keycodes.rs
@@ -1,18 +1,6 @@
 use ncurses::*;
 use std::collections::{HashMap, HashSet};
 
-pub const KEY_E: i32 = 0x65;
-pub const KEY_Q: i32 = 0x71;
-pub const KEY_TAB: i32 = 0x09;
-pub const KEY_RETURN: i32 = 0x0a;
-pub const KEY_S: i32 = 0x73;
-pub const KEY_W: i32 = 0x77;
-pub const KEY_D: i32 = 0x64;
-pub const KEY_A: i32 = 0x61;
-pub const KEY_I: i32 = 0x69;
-pub const KEY_J: i32 = 0x4A;
-pub const KEY_K: i32 = 0x4B;
-pub const KEY_SHIFT_I: i32 = 0x49;
 pub const KEY_ESCAPE: i32 = 0x1B;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
