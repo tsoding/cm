8b9987ffb13b51a8db967061c0ab241014c5c9b6
rexim <reximkut@gmail.com>
2020-09-14 19:52:25 +0700

    Add TODO(#185)

--- src/cm/output_buffer.rs
+++ src/cm/output_buffer.rs
@@ -211,6 +211,7 @@ impl OutputBuffer {
                     Ok(0) => break,
                     Ok(_) => {
                         if let Some(list) = self.lists.last_mut() {
+                            // TODO(#185): move the tab expansion to ItemList so it's available for every list-like component
                             list.items.push(expand_tabs(&line, TABSIZE() as usize));
                             changed = true;
                         }
